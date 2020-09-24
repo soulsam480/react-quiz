4dd95991f6569579f3d4b6bc999bfcf1717ad5c6
sambit sahoo <soulsam480@hotmail.com>
2020-09-24 23:37:05 +0530

    Updates

---
+++ precache-manifest.ed779fc8efe4062f58eaf5cbe8c79ca9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ada37411163eb5d5143f76bcb55090bf",
+    "url": "/react-quiz/index.html"
+  },
+  {
+    "revision": "e25c52129911c8e68599",
+    "url": "/react-quiz/static/js/2.f2901fb0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-quiz/static/js/2.f2901fb0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2bc0befb41404016d9c3",
+    "url": "/react-quiz/static/js/main.3ce6347e.chunk.js"
+  },
+  {
+    "revision": "928f8d10e2d93c26f2dd",
+    "url": "/react-quiz/static/js/runtime-main.acabde0b.js"
+  }
+]);
